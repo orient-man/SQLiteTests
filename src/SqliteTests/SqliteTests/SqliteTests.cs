1b28c290085f84e1f8370af4ae875255429e65b0
orientman <orientman@gmail.com>
2016-03-03 21:51:46 +0100

    1st test

---
+++ src/SqliteTests/SqliteTests/SqliteTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace SqliteTests
+{
+    [TestFixture]
+    public class SqliteTests
+    {
+        [Test]
+        public void ShouldDoTheRightThing()
+        {
+            // arrange
+
+            // act
+            // assert
+        }
+    }
+}
